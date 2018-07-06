0744a8d10c9895dc6cde2d953dc81712650b8664
Denis Odegov <odegov_denis@mail.ru>
2018-07-06 16:55:57 +0600

    К сожалению, на этой неделе у меня было крайне мало времени для решения ДЗ.
    Прикрепляю то, что успел сделать.

---
+++ ДЗ-2.e3.py
@@ -0,0 +1,18 @@
+_author_ = 'Одегов Денис Юрьевич'
+
+print('Введите список из целых чисел: ')
+a = [input()]
+
+b = []
+
+n = 0
+while n < len(a):
+	if a[n] % 2 == 0 :
+		b.insert(n, (a[n] / 4)
+		else: 
+			b.insert(n, (a[n] * 2)
+n = n + 1
+
+
+print('Новый список: ', b)
+
